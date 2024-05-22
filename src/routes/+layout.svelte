0715ba70a24ec8db03ef58894e734e04c0511987
DibyajyotiDey <dibyajyoti49@outlook.com>
2024-05-22 11:29:06 +0530

    fixed styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
     />
-    <link rel="stylesheet" href="./styles.css">
+
 
     <!--	<meta name="description" content="Svelte demo app" />-->
 </svelte:head>
