9ccbb8b62ffaefb6cdf1e4c0e9c27f7a3544044d
DibyajyotiDey <dibyajyoti49@outlook.com>
2024-05-22 11:26:39 +0530

    fixed styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,12 @@
 </script>
 <svelte:head>
     <title>LinkedIn</title>
-<!--    <link rel="stylesheet" href="./styles.css">-->
     <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
     />
+    <link rel="stylesheet" href="./styles.css">
+
     <!--	<meta name="description" content="Svelte demo app" />-->
 </svelte:head>
 <NavBar></NavBar>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  important: true,
   theme: {
     extend: {
       colors: {
@@ -17,4 +18,5 @@ export default {
   },
   plugins: [],
 }
+
 
